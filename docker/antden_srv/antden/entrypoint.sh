900e63cd95ec288c1a953003f9a94b72326ce0b2
lijinfeng2011 <lijinfeng2011@gmail.com>
2020-07-14 20:17:25 +0800

    After starting the antden service, add the local machine to the antden group

--- docker/antden_srv/antden/entrypoint.sh
+++ docker/antden_srv/antden/entrypoint.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+/opt/mydan/dan/bootstrap/bin/bootstrap --start
+
+echo MYDan started.
+
 /opt/AntDen/bin/antden slave c --start
 /opt/AntDen/bin/antden c c --start
 /opt/AntDen/bin/antden s c --start
 /opt/AntDen/bin/antden d start
+echo AntDen started.
 
-/opt/mydan/dan/bootstrap/bin/bootstrap --start
+sleep 3
+
+/opt/AntDen/bin/antden s am 127.0.0.1 --group antden
+echo addMachine 127.0.0.1 done.
 
 sleep 100000000000000
