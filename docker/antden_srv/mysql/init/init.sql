2e5b00e2a7eaa9fdb4abde212bd1293b7bf205f1
lijinfeng2011 <lijinfeng2011@gmail.com>
2020-07-14 19:07:45 +0800

    Database table initialization about docker running antden service

--- docker/antden_srv/mysql/init/init.sql
+++ docker/antden_srv/mysql/init/init.sql
@@ -1,5 +1,7 @@
 create database antden;
 
+use antden;
+
 CREATE TABLE `user` (
     `id` int(32) unsigned NOT NULL AUTO_INCREMENT,
     `name` varchar(255) NOT NULL,
@@ -7,3 +9,14 @@ CREATE TABLE `user` (
     PRIMARY KEY (`id`)
 );
 insert into user (`name`,`isadmin`)values('antden','2');
+
+CREATE TABLE `auth` (
+  `id` int(32) unsigned NOT NULL AUTO_INCREMENT,
+  `user` varchar(255) NOT NULL,
+  `group` varchar(255) NOT NULL,
+  `executer` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+insert into auth (`user`,`group`,`executer`)values('antden','antden','cmd');
+insert into auth (`user`,`group`,`executer`)values('antden','foo','cmd');
