1bf850c440fb35bf542a432ab64e564dde1b8547
lijinfeng2011 <lijinfeng2011@gmail.com>
2020-07-17 09:24:52 +0800

    run.sh Start elk when elk is not empty

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+os=$(uname)
+echo "os: $os"
+if [ "X$os" != 'XLinux'  ];then
+    echo "Not supported you os, Only allowed to run on centos7"
+    exit
+fi
+
 
 os=$(cat /etc/redhat-release|sed -r 's/(\w+)\s+.* ([0-9]+)\..*/\1\2/')
 
@@ -8,7 +15,7 @@ if [ "X$os" != 'XCentOS7'  ];then
     exit
 fi
 
-docker --version 2>/dev/null 
+docker --version 2>/dev/null
 if [ $? != 0 ];then
     curl -sL get.docker.io|bash
     service docker start
@@ -38,4 +45,8 @@ fi
 
 cd AntDen || exit 1
 
-./control srv up
+./control srv up -d
+
+if [ "X$elk" != "X" ]; then
+    ./control srv up -d
+fi
